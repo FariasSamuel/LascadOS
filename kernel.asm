e65589627314f8f88108d07f61a8128b3d47bcff
FariasSamuel <samuelfabrasil@gmail.com>
2025-02-20 21:46:58 -0300

    create file

--- kernel.asm
+++ kernel.asm
@@ -3,14 +3,12 @@ ORG 0x1000
 
 start:
     ; Set up data segments
-
-    mov byte [0x3000],0; maximum number of files
-    mov word [0x3001],0x3241; first available byte
+    mov byte [0x3000], 0      ; file count = 0 (max 20 files)
+    mov word [0x3001], 0x3241 ; pointer to first available byte
 
     mov ax, 0
     mov ds, ax
     mov es, ax
-
 
     ; Print welcome message
     mov si, welcome_msg
@@ -32,9 +30,14 @@ command_loop:
     je do_help
 
     mov si, command_buffer
-    mov di, crt_file
+    mov di, cmd_create
     call strcmp
     je create_file
+
+    mov si, command_buffer
+    mov di, cmd_list
+    call strcmp
+    je do_list
 
     mov si, command_buffer
     mov di, cmd_clear
@@ -45,11 +48,6 @@ command_loop:
     mov di, cmd_reboot
     call strcmp
     je do_reboot
-
-    mov si, command_buffer
-    mov di, cmd_time
-    call strcmp
-    je do_time
 
     mov si, command_buffer
     mov di, cmd_shutdown
@@ -83,45 +81,19 @@ do_reboot:
     int 0x16
     jmp 0xFFFF:0x0000
 
-do_time:
-    mov ah, 0x02    ; Get RTC time
-    int 0x1A        ; BIOS call
-
-    mov si, time_msg
-    call print_string
-
-    ; Convert and print hour
-    mov al, ch
-    call print_bcd
-    mov al, ':'
-    call print_char
-
-    ; Convert and print minute
-    mov al, cl
-    call print_bcd
-    mov al, ':'
-    call print_char
-
-    ; Convert and print second
-    mov al, dh
-    call print_bcd
-    
-    call new_line
-    jmp command_loop
-
 do_shutdown:
     cli             ; Disable interrupts
     hlt             ; Halt the CPU
+    jmp $
 
-; Get available memory in KB - Fixed version
 do_mem:
-    int 0x12        ; Call BIOS memory size function
-    push ax         ; Save the result (in KB)
+    int 0x12        ; BIOS memory size (KB)
+    push ax         ; Save memory size
     
     mov si, mem_msg
     call print_string
     
-    pop ax          ; Restore the memory size
+    pop ax          ; Restore memory size
     call print_decimal
     
     mov si, kb_msg
@@ -129,18 +101,224 @@ do_mem:
     call new_line
     jmp command_loop
 
+
+
+
+create_file:
+    push ax
+    push bx
+    push cx
+    push dx
+
+    ; Check if maximum files reached (20)
+    mov al, [0x3000]
+    cmp al, 20
+    je .no_more_files
+
+    ; Calculate entry address (13 bytes per entry)
+    xor ah, ah         ; Clear high part of AX
+    mov bl, 13          ; Entry size
+    mul bl              ; AX = file_count * 13
+    add ax, 0x3005      ; Base address of file table
+    mov bx, ax          ; BX = entry address
+
+    ; Get filename
+    mov si, filename_prompt
+    call print_string
+    mov di, command_buffer
+    call read_string
+
+    ; Copy filename (max 10 chars, zero-padded)
+    mov cx, 9          ; Maximum filename length
+    mov si, command_buffer
+    push bx             ; Save entry address
+    mov di, bx          ; Destination for filename
+.copy_name:
+    lodsb
+    test al, al         ; Check for end of string
+    jz .pad_name
+    mov [di], al
+    inc di
+    loop .copy_name
+    jmp .name_done
+.pad_name:
+    mov al, 0           ; Pad with zeros
+.pad_loop:
+    mov [di], al
+    inc di
+    loop .pad_loop
+.name_done:
+    pop bx
+    ; Get and validate file size
+    mov si, size_prompt
+    call print_string
+    call read_decimal
+    mov ax, bx                   
+    push ax
+    mov bx, [0x3001]               
+    add ax, bx                      
+    cmp ax, 0x3629                  
+    jge .invalid_size             
+
+    mov al, [0x3000]
+    xor ah, ah         
+    mov bl, 13         
+    mul bl              
+    add ax, 0x3005      
+    mov bx, ax     
+    pop ax  
+    mov [bx + 9], ax   ; Store at offset 9
+
+    mov ax,[bx+9]
+    call print_decimal
+
+    ; Calculate and store file address
+    mov ax, [0x3001]    ; Current data pointer
+    mov [bx + 11], ax   ; Store at offset 11
+
+    add ax,[bx+9]
+    mov [0x3001],ax
+
+    ; Increment file count
+    inc byte [0x3000]
+
+    ; Success
+    mov si, success
+    call print_string
+    
+    pop dx
+    pop cx
+    pop bx
+    pop ax
+    jmp command_loop
+
+.invalid_size:
+    mov si, invalid_size_msg
+    jmp .error
+
+.too_large:
+    mov si, too_large_msg
+    jmp .error
+
+.no_more_files:
+    mov si, files_error
+    jmp .error
+
+.error:
+    call print_string
+    pop dx
+    pop cx
+    pop bx
+    pop ax
+    jmp command_loop
+
+.temp_size: dw 0
+
+do_list:
+    push ax
+    push bx
+    push cx
+    push dx
+
+    ; Check for files
+    mov al, [0x3000]
+    test al, al
+    jz .no_files
+
+    ; Print header
+    mov si, list_header
+    call print_string
+
+    ; List all files
+    xor cx, cx          ; File counter
+    mov bx, 0x3005      ; Start of file table
+.list_loop:
+    mov al, [0x3000]    ; Get total files
+    cmp cl, al
+    jae .done           ; If we've listed all files, done
+
+    ; Print file number
+    push cx             ; Save file counter
+    inc cx              ; Start from 1
+    mov ax, cx
+    call print_decimal
+    mov si, list_separator
+    call print_string
+
+    ; Print filename
+    mov dx, cx          ; Save file number
+    mov cx, 9          ; Maximum filename length
+.name_loop:
+    mov al, [bx]
+    test al, al
+    jz .name_done
+    call print_char
+    inc bx
+    loop .name_loop
+.name_done:
+    add bx, cx          ; Skip any remaining filename bytes
+    
+    ; Print size
+    mov si, list_size
+    call print_string
+    xor ax, ax
+    mov ax, [bx]        ; Get file size
+    call print_decimal
+    inc bx
+
+    ; Print address
+    mov si, list_addr
+    call print_string
+    mov ax, [bx + 1]    ; Get file address (2 bytes)
+    call print_decimal
+    call new_line
+    inc bx
+
+    ; Move to next entry (reset BX to start + entry_size * file_number)
+    pop cx              ; Restore file counter
+    inc cx              ; Next file
+    push cx             ; Save for next iteration
+    
+    mov ax, 13          ; Entry size
+    mul cl              ; AX = entry_size * file_number
+    add ax, 0x3005      ; Add base address
+    mov bx, ax          ; BX points to next entry
+
+    pop cx              ; Restore counter for loop
+    jmp .list_loop
+
+.done:
+    mov si, total_files
+    call print_string
+    xor ax, ax
+    mov al, [0x3000]
+    call print_decimal
+    mov si, files_suffix
+    call print_string
+    jmp .exit
+
+.no_files:
+    mov si, no_files_msg
+    call print_string
+
+.exit:
+    pop dx
+    pop cx
+    pop bx
+    pop ax
+    jmp command_loop
 ; Function to read a string from keyboard
 read_string:
     xor cx, cx
 .loop:
     mov ah, 0
     int 0x16
-    cmp al, 13      ; Check for Enter key
+    cmp al, 13      ; Enter key
     je .done
-    cmp al, 8       ; Check for Backspace
+    cmp al, 8       ; Backspace
     je .backspace
-    cmp cx, 62      ; Check buffer limit
-    jae .loop
+    cmp cx, 62      ; Buffer limit
+    jge .loop
     stosb
     inc cx
     mov ah, 0x0E    ; Echo character
@@ -165,8 +343,26 @@ read_string:
     call new_line
     ret
 
-; Function to compare two strings
+read_decimal:
+    mov di,command_buffer
+    call read_string
+    xor ax,ax
+    mov si,command_buffer
+    mov bx,0
+    mov al,0
+.convert:
+    imul bx,10
+    mov al,[si]
+    sub al,'0'
+    add bx,ax 
+    inc si
+    loop .convert
+    ret
+
+; Function to compare two strings (pointed by SI and DI)
 strcmp:
+    push ax
+    push bx
 .loop:
     mov al, [si]
     mov bl, [di]
@@ -178,13 +374,17 @@ strcmp:
     inc di
     jmp .loop
 .not_equal:
+    pop bx
+    pop ax
     clc
     ret
 .equal:
+    pop bx
+    pop ax
     stc
     ret
 
-; Function to print a string
+; Function to print a string (pointed by SI)
 print_string:
     mov ah, 0x0E
 .loop:
@@ -196,7 +396,7 @@ print_string:
 .done:
     ret
 
-; Function to print a single character
+; Function to print a single character (in AL)
 print_char:
     mov ah, 0x0E
     int 0x10
@@ -210,21 +410,7 @@ new_line:
     call print_char
     ret
 
-; Function to print BCD number
-print_bcd:
-    push ax
-    mov ah, al
-    shr al, 4
-    and ah, 0x0F
-    add al, '0'
-    add ah, '0'
-    call print_char
-    mov al, ah
-    call print_char
-    pop ax
-    ret
-
-; Function to print a decimal number
+; Function to print a decimal number (value in AX)
 print_decimal:
     push ax
     push bx
@@ -235,18 +421,17 @@ print_decimal:
     xor cx, cx      ; Counter for digits
     
 .divide_loop:
-    xor dx, dx      ; Clear high word before division
-    div bx          ; Divide by 10
-    push dx         ; Save remainder
-    inc cx          ; Increment digit counter
-    test ax, ax     ; Check if quotient is zero
+    xor dx, dx      ; Clear DX before division
+    div bx          ; Divide AX by 10
+    push dx         ; Save remainder (digit)
+    inc cx          ; Count digits
+    test ax, ax     ; If quotient is 0, finish
     jnz .divide_loop
     
 .print_loop:
     pop ax          ; Get digit
     add al, '0'     ; Convert to ASCII
-    mov ah, 0x0E    ; BIOS teletype output
-    int 0x10
+    call print_char
     loop .print_loop
     
     pop dx
@@ -255,123 +440,53 @@ print_decimal:
     pop ax
     ret
 
-; Function to clear screen
+; Function to clear the screen
 clear_screen:
     mov ah, 0x00
     mov al, 0x03
     int 0x10
     ret
 
-read_decimal:
-    mov di,command_buffer
-    call read_string
-    xor ax,ax
-    mov si,command_buffer
-    mov ah,0x0E
-    mov bx,0
-    mov al,0
-.convert:
-    imul bx,10
-    mov al,[si]
-    sub al,'0'
-    add bh,al 
-    inc si
-    loop .convert
-    ret
-create_file:
-    push bx ;verifica se tem espaço na tabela
-    mov bx,[0x3000]
-    cmp bx, 20
-    je .no_more_files
-    
-    mov di, command_buffer;pede o nome do arquivo
-    call read_string
-
-    mov si, command_buffer;salva o arquivo na tabela
-    imul bx,13
-    add bx,0x3005
-.retry:
-    mov al, [si]
-    mov [bx], al
-    inc si
-    inc bx
-    loop .retry
-
-    call read_decimal;pede quantidade de bytes
-    mov al,bh;salva a quantidade na tabela
-    xor bx,bx
-    mov bx,[0x3000]
-    imul bx,13
-    add bx,0x3005
-    add bx, 10
-    mov [bx],al
-    mov cl, al
-
-    inc bx;salva inicio do arquivo
-    mov ax,[0x3001]
-    mov [bx],ax
-
-    mov cx,13
-    mov bx,[0x3000]
-    imul bx,13
-    add bx,0x3005
-    mov ah,0x0E
-.repetir:
-    mov al,[bx]
-    int 0x10
-    inc bx
-    loop .repetir
-
-    jmp .done
-.no_more_files:
-    mov si,files_error
-    call print_string
-    ret
-
-.no_more_bytes:
-    mov si,bytes_error
-    call print_string
-    ret
-
-.done:
-    mov si,sucess
-    call print_string
-    ret
-
-
 ; Data section
-welcome_msg db 'Interactive Kernel v0.2', 13, 10
-           db 'Type "help" for commands', 13, 10, 0
-prompt db '> ', 0
-help_msg db 'Available commands:', 13, 10
-        db '  help      - Show this help', 13, 10
-        db '  clear     - Clear screen', 13, 10
-        db '  reboot    - Restart system', 13, 10
-        db '  time      - Show current time', 13, 10
-        db '  shutdown  - Halt the CPU', 13, 10
-        db '  mem       - Show available memory', 13, 10, 0
-unknown_cmd db 'Unknown command. Type "help" for available commands.', 13, 10, 0
-reboot_msg db 'System will reboot. Press any key...', 13, 10, 0
-time_msg db 'Current Time: ', 0
-mem_msg db 'Available memory: ', 0
-kb_msg db ' KB', 0
-sucess db 'YAAAAAS, pisa prima.', 13, 10, 0
-files_error db 'There isnt available space for files.', 13, 10, 0
-bytes_error db 'There isnt available space for files.', 13, 10, 0
+welcome_msg     db 'Interactive Kernel v0.2', 13, 10
+                db 'Type "help" for commands', 13, 10, 0
+prompt          db '> ', 0
+help_msg        db 'Available commands:', 13, 10
+                db '  help      - Show this help', 13, 10
+                db '  create    - Create a file', 13, 10
+                db '  list      - List all files', 13, 10
+                db '  clear     - Clear screen', 13, 10
+                db '  reboot    - Restart system', 13, 10
+                db '  shutdown  - Halt the CPU', 13, 10
+                db '  mem       - Show available memory', 13, 10, 0
+unknown_cmd     db 'Unknown command. Type "help" for available commands.', 13, 10, 0
+reboot_msg      db 'System will reboot. Press any key...', 13, 10, 0
+mem_msg         db 'Available memory: ', 0
+kb_msg          db ' KB', 0
+
+; File system messages
+filename_prompt db 'Enter file name: ', 0
+size_prompt     db 'Enter file size (bytes): ', 0
+invalid_size_msg db 'Invalid file size.', 13, 10, 0
+too_large_msg   db 'File too large (max 1000 bytes).', 13, 10, 0
+files_error     db "Maximum number of files reached.", 13, 10, 0
+success         db 'File created successfully.', 13, 10, 0
+list_header     db 'Files:', 13, 10, 0
+list_separator  db '. ', 0
+list_size       db ' - ', 0
+list_addr       db ' bytes at ', 0
+total_files     db 'Total files: ', 0
+files_suffix    db ' file(s)', 13, 10, 0
+no_files_msg    db 'No files found.', 13, 10, 0
 
 ; Command strings
-cmd_help db 'help', 0
-cmd_clear db 'clear', 0
-cmd_reboot db 'reboot', 0
-cmd_time db 'time', 0
-crt_file db 'create', 0
-cmd_mem db 'mem', 0
-dlt_file db 'delete',0
-read_file db 'delete',0
-cmd_shutdown db 'shutdown', 0
+cmd_help        db 'help', 0
+cmd_clear       db 'clear', 0
+cmd_reboot      db 'reboot', 0
+cmd_create      db 'create', 0
+cmd_list        db 'list', 0
+cmd_shutdown    db 'shutdown', 0
+cmd_mem         db 'mem', 0
 
 ; Buffer for user input
-command_buffer times 64 db 0
-
-; Pad to two full sectors
-;times 1080-($-$$) db 0
+command_buffer  times 64 db 0
